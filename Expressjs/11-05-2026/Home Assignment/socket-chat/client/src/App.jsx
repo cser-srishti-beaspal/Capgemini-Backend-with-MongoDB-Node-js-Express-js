8e0b12e6698e92ada947b108e2d5a9551cb0c797
cser-srishti-beaspal <cser.srishti.beaspal@gmail.com>
2026-05-11 23:52:06 +0530

    socket.io and e-commerce

---
+++ Expressjs/11-05-2026/Home Assignment/socket-chat/client/src/App.jsx
@@ -0,0 +1,15 @@
+import Chat from "./Chat";
+
+export default function App() {
+  return (
+    <div style={{
+      height:"100vh",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      background:"#0f0f13"
+    }}>
+      <Chat />
+    </div>
+  );
+}
